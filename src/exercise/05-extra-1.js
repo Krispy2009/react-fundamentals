8cccd15184a95b479a6d18df67aaf1c159fd818f
Kristi Achilleos <krispy2009@gmail.com>
2021-09-28 19:34:34 +0300

    finish extra credit for ex5

---
+++ src/exercise/05-extra-1.js
@@ -0,0 +1,52 @@
+// Styling
+// http://localhost:3000/isolated/exercise/05.js
+
+import * as React from 'react'
+import '../box-styles.css'
+
+// 💰 Use the className for the size and style (backgroundColor, fontStyle) for the color and the font style
+// 💰 each of the elements should also have the "box" className applied
+
+// 🐨 add a className prop to each of these and apply the correct class names
+// 💰 Here are the available class names: box, box--large, box--medium, box--small
+
+// 🐨 add a style prop to each of them as well so their background color
+// matches what the text says it should be as well as `fontStyle: 'italic'`
+
+const Box = ({style, className, children}) => {
+  const styles = {...style, fontStyle: 'italic'}
+  const classNames = `box ${className}`
+  return (
+    <div style={styles} className={classNames}>
+      {children}
+    </div>
+  )
+}
+
+const smallBox = (
+  <Box className="box--small" style={{backgroundColor: 'lightblue'}}>
+    small lightblue box
+  </Box>
+)
+const mediumBox = (
+  <Box className="box--medium" style={{backgroundColor: 'pink'}}>
+    medium pink box
+  </Box>
+)
+const largeBox = (
+  <Box className="box--large" style={{backgroundColor: 'orange'}}>
+    large orange box
+  </Box>
+)
+
+function App() {
+  return (
+    <div>
+      {smallBox}
+      {mediumBox}
+      {largeBox}
+    </div>
+  )
+}
+
+export default App
